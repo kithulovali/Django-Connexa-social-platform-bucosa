e618f13c3fc4962ba9826b0c9b668e86ce50fb38
kithulovali <kithulovalibin@gmail.com>
2025-08-03 21:28:47 +0300

    structure  resolution

--- bucosa/staticfiles/js/firebase-push.js
+++ bucosa/staticfiles/js/firebase-push.js
@@ -1,64 +1,67 @@
-    
-      const firebaseConfig = {
-        apiKey: "AIzaSyAsXHxxto0D-0nZ2I7sIhHFDjCAgurUoZM",
-        authDomain: "bucosa-5ccde.firebaseapp.com",
-        projectId: "bucosa-5ccde",
-        storageBucket: "bucosa-5ccde.appspot.com",
-        messagingSenderId: "981676237571",
-        appId: "1:981676237571:web:1a7445524217e5ae917f36",
-        measurementId: "G-XNJBM8KB53"
-      };
-      firebase.initializeApp(firebaseConfig);
+// static/js/firebase-push.js
 
-      const messaging = firebase.messaging();
+const firebaseConfig = {
+  apiKey: "AIzaSyAsXHxxto0D-0nZ2I7sIhHFDjCAgurUoZM",
+  authDomain: "bucosa-5ccde.firebaseapp.com",
+  projectId: "bucosa-5ccde",
+  storageBucket: "bucosa-5ccde.appspot.com",
+  messagingSenderId: "981676237571",
+  appId: "1:981676237571:web:1a7445524217e5ae917f36",
+  measurementId: "G-XNJBM8KB53"
+};
 
-      function sendTokenToBackend(token) {
-        if (localStorage.getItem('fcm_token') !== token) {
-          fetch('/save_fcm_token/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: 'token=' + encodeURIComponent(token)
-          }).then(() => {
-            localStorage.setItem('fcm_token', token);
-          });
-        }
-      }
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-      function getAndSendToken() {
-        Notification.requestPermission().then((permission) => {
-          if (permission === 'granted') {
-            messaging.getToken({ vapidKey: 'BIrGRQ1nsd0Cf7Fpmd-qMKhkqLMUaXXXpBUpkPc908eBgSN3ApVrSkQuLvS6Phz8UVLT3QlYSZ0RyIQBjbAuhFc' })
-              .then((token) => {
-                if (token) {
-                  sendTokenToBackend(token);
-                } else {
-                  console.log('No registration token available.');
-                }
-              })
-              .catch((err) => {
-                console.log('Unable to get token.', err);
-              });
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function sendTokenToBackend(token) {
+  if (localStorage.getItem('fcm_token') !== token) {
+    fetch('/save_fcm_token/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-CSRFToken': getCookie('csrftoken')  // only if CSRF is required
+      },
+      body: 'token=' + encodeURIComponent(token)
+    }).then(() => {
+      localStorage.setItem('fcm_token', token);
+    }).catch(err => {
+      console.error('Failed to send token to backend:', err);
+    });
+  }
+}
+
+function getAndSendToken() {
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      navigator.serviceWorker.ready.then((registration) => {
+        messaging.getToken({
+          vapidKey: 'BIrGRQ1nsd0Cf7Fpmd-qMKhkqLMUaXXXpBUpkPc908eBgSN3ApVrSkQuLvS6Phz8UVLT3QlYSZ0RyIQBjbAuhFc',
+          serviceWorkerRegistration: registration
+        })
+        .then((token) => {
+          if (token) {
+            console.log('FCM Token:', token);
+            sendTokenToBackend(token);
           } else {
-            console.log('Notification permission not granted.');
+            console.log('No registration token available.');
           }
+        })
+        .catch((err) => {
+          console.error('Unable to get token.', err);
         });
-      }
-
-      window.addEventListener('load', getAndSendToken);
-
-      messaging.onTokenRefresh(() => {
-        messaging.getToken({ vapidKey: 'BIrGRQ1nsd0Cf7Fpmd-qMKhkqLMUaXXXpBUpkPc908eBgSN3ApVrSkQuLvS6Phz8UVLT3QlYSZ0RyIQBjbAuhFc' })
-          .then((refreshedToken) => {
-            sendTokenToBackend(refreshedToken);
-          })
-          .catch((err) => {
-            console.log('Unable to retrieve refreshed token ', err);
-          });
       });
+    } else {
+      console.log('Notification permission not granted.');
+    }
+  });
+}
 
-      messaging.onMessage((payload) => {
-        alert('Push notification: ' + payload.notification.title + ' - ' + payload.notification.body);
-      });
-    
+if (window.USER_IS_AUTHENTICATED === true) {
+  getAndSendToken();
+}
