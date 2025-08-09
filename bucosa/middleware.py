2c96eb3ea2e94e3106d051d8585d91d01474282c
kithulovali <kithulovalibin@gmail.com>
2025-08-09 17:36:39 +0300

    mid fixes

--- bucosa/middleware.py
+++ bucosa/middleware.py
@@ -1,3 +1,17 @@
+from django.shortcuts import redirect
+from django.conf import settings
+from django.urls import reverse
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Only redirect if not authenticated and not accessing exempt URLs
+        exempt_urls = getattr(settings, 'LOGIN_EXEMPT_URLS', [reverse('account_login'), reverse('account_signup')])
+        if not request.user.is_authenticated and request.path not in exempt_urls:
+            return redirect(settings.LOGIN_URL)
+        return self.get_response(request)
 from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import reverse
