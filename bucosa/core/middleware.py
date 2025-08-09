8b825bb7d5d2b6debf6db650d4902d995c6c8865
kithulovali <kithulovalibin@gmail.com>
2025-08-09 17:58:15 +0300

    mid fixes final

--- bucosa/core/middleware.py
+++ bucosa/core/middleware.py
@@ -25,30 +25,3 @@ class LoginRequiredMiddleware:
             if not any(m.match(path) for m in EXEMPT_URLS):
                 return redirect(settings.LOGIN_URL + f'?next={request.path}')
         return self.get_response(request)
-from django.conf import settings
-from django.shortcuts import redirect
-from django.urls import reverse
-import re
-
-EXEMPT_URLS = [
-    re.compile(reverse('users:login').lstrip('/')),
-    re.compile(reverse('users:logout').lstrip('/')),
-    re.compile(reverse('users:register').lstrip('/')),
-    re.compile(r'^admin/'),
-    re.compile(r'^accounts/'),
-    re.compile(r'^static/'),
-    re.compile(r'^media/'),
-    re.compile(r'^manifest.json$'),
-    re.compile(r'^service-worker.js$'),
-]
-
-class LoginRequiredMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        path = request.path_info.lstrip('/')
-        if not request.user.is_authenticated:
-            if not any(m.match(path) for m in EXEMPT_URLS):
-                return redirect(settings.LOGIN_URL + f'?next={request.path}')
-        return self.get_response(request)
