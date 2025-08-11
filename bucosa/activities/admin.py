8bdcee394298f5050161e93e99ddea8878e65673
kithulovali <kithulovalibin@gmail.com>
2025-08-11 18:01:56 +0300

    anouncement on admin

--- bucosa/activities/admin.py
+++ bucosa/activities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Event , Post , Comment
+from . models import Event , Post , Comment , Announcement
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Announcement)
