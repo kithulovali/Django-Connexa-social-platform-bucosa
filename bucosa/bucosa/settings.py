4e7541f8ec9f280e8ecdab6d7824e7b4736ce299
kithulovali <kithulovalibin@gmail.com>
2025-08-11 15:12:49 +0300

    celary and background redis

--- bucosa/bucosa/settings.py
+++ bucosa/bucosa/settings.py
@@ -1,9 +1,4 @@
-# Celery configuration for Redis broker
-CELERY_BROKER_URL = os.environ.get('REDIS_URL')
-CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+
 
 import os
 from pathlib import Path
@@ -259,3 +254,9 @@ CHANNEL_LAYERS = {
     },
 }
 
+# Celery configuration for Redis broker
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
