dd3e09500f0f58a4a1d000e5f6565b63a863b15f
kithulovali <kithulovalibin@gmail.com>
2025-08-13 16:25:24 +0300

    default welcome message and email

--- bucosa/bucosa/routing.py
+++ bucosa/bucosa/routing.py
@@ -1,12 +1,12 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
+from django.urls import re_path
 from notifications.consumers import NotificationConsumer
 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            path("ws/notifications/", NotificationConsumer.as_asgi()),
+            re_path(r"ws/notifications/(?P<user_id>\d+)/", NotificationConsumer.as_asgi()),
         ])
     ),
 })
