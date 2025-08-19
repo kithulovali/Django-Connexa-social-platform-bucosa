03316d4a0067db18df5a21dec9bb68b262e7d5d3
kithulovali <kithulovalibin@gmail.com>
2025-08-19 12:44:59 +0300

    finalisation

--- bucosa/users/templatetags/user_extras.py
+++ bucosa/users/templatetags/user_extras.py
@@ -7,10 +7,13 @@ register = template.Library()
 def get_item(dictionary, key):
     return dictionary.get(key, 0)
 
-# New filter to get user by id
 @register.filter
 def user_by_id(user_id):
     try:
         return User.objects.get(id=user_id)
     except User.DoesNotExist:
         return None
+
+@register.filter
+def split(value, delimiter=','):
+    return value.split(delimiter)
