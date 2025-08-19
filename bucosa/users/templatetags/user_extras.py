4929d160185c40452227d91714b641eed8426cea
kithulovali <kithulovalibin@gmail.com>
2025-08-19 12:38:49 +0300

    finalisation

--- bucosa/users/templatetags/user_extras.py
+++ bucosa/users/templatetags/user_extras.py
@@ -1,7 +1,16 @@
 from django import template
+from django.contrib.auth.models import User
 
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, 0)
+
+# New filter to get user by id
+@register.filter
+def user_by_id(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return None
