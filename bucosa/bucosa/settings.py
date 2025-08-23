8fe4e13f94cf6f9673e5485a2923101e725f1a1b
kithulovali <kithulovalibin@gmail.com>
2025-08-23 19:10:45 +0300

    resolving slowdowns and fast AGSI loading

--- bucosa/bucosa/settings.py
+++ bucosa/bucosa/settings.py
@@ -256,8 +256,10 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [os.environ.get("REDIS_URL")],
-            "capacity": 3000,  # Increase capacity for better handling of concurrent connections
-            "expiry": 30,  # Increase expiry time to 30 seconds
+            "capacity": 3000,
+            "expiry": 30,
+            "socket_timeout": 10,
+            "socket_connect_timeout": 10,
         },
     },
 }
